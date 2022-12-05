78c6e5417e3bd33de7f68686b698de8424c4c962
Ireke Ukiwo Samuel <48257726+samuelireke@users.noreply.github.com>
2022-12-05 15:57:07 +0000

    added responsiveness to form

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import Vuetify from 'vuetify'
-// import 'vuetify/dist/vuetify.min.css'
 
-// import './assets/main.css'
+// Vuetify
+// import 'vuetify/styles'
+// import { createVuetify } from 'vuetify'
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
+// import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+// const vuetify = createVuetify({
+//   components,
+//   directives,
+//   icons: {
+//     defaultSet: 'mdi',
+//     aliases,
+//     sets: {
+//       mdi,
+//         }
+//     }
+// })
 
 const app = createApp(App)
 
 app.use(router)
-// app.use(Vuetify)
+// app.use(vuetify)
 
 app.mount('#app')
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,24 +1,34 @@
 <template>
-  <div>
-    <h1>Register</h1>
-
-  <input 
-    type="email"
-    name="email"
-    v-model="email"
-    placeholder="email" />
-    <br>
-    <input 
-    type="password"
-    name="password"
-    v-model="password"
-    placeholder="password" />
-    <br>
-    <div class="error" v-html="error" />
-    <br>
-    <button @click="register">
-      Register
-    </button>
+  <div class="container">
+    <div class="form">
+      <div class="flex-container">
+        <div class="flex-container-item">
+          <h1>Register</h1>
+        </div>
+        <div class="flex-container-item">
+          <input 
+          type="email"
+          name="email"
+          v-model="email"
+          placeholder="email" />
+          </div>
+          <br>
+          <div class="flex-container-item">
+          <input 
+          type="password"
+          name="password"
+          v-model="password"
+          placeholder="password" />
+          </div>
+          <div class="error" v-html="error" />
+          <div class="flex-container-item">
+          <button 
+            @click="register">
+            REGISTER
+          </button>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -49,7 +59,50 @@ export default {
 }
 </script>
 <style scoped>
+.container { 
+  background-color: #efefef;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+.form { 
+  background-color: #fafafa;
+  padding: 2rem;
+  width: 400px
+}
+.flex-container-item{
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 10px;
+  justify-content: center;
+}
+.flex-container-item button{
+  /* width: 100%; */
+  background-color: cyan;
+  border: 0;
+  font-weight: bold;
+  padding: 10px;
+}
+.flex-container-item button:hover {
+  cursor: pointer;
+  background-color: rgb(5, 216, 216);
+}
+
+.flex-container-item h1{
+  display: flex;
+  justify-content: center;
+  background-color: cyan;
+  width: 100%;
+}
 .error {
   color:red;
+}
+
+/* responsiveness */
+@media all and (max-width:480px) {
+  .flex-container-item {
+    flex-direction: column;
+  }
 }
 </style>
